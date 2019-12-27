ac0921d91455b73ae8f3bfe84dcbca973ededf04
chenzhongliang <171082223@qq.com>
2019-12-27 17:03:38 +0800

    '第二次PUSH'

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  configureWebpack:{
+    resolve:{
+      alias:{
+      'assets':'@/assets',
+      'common':'@/assets',
+      'component':'@/assets',
+      'network':'@/assets',
+      'views':'@/assets',}
+    }
+  }
+
+}
